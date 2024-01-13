a98d7f0ec99369dce34dd8497264123e33995dbc
Matthew Stanciu <mattbstanciu@gmail.com>
2024-01-13 18:30:05 -0500

    add shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center gap-y-24 p-24">
       <h1 className="font-bold text-amber-400 text-6xl">Passport Data Pages</h1>
     </main>
   );
