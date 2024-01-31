aa816c1c7976807ed44edaf0b76e0369ab4ecd09
Matthew Stanciu <mattbstanciu@gmail.com>
2024-01-31 11:42:55 -0500

    idk

--- app/page.tsx
+++ app/page.tsx
@@ -21,6 +21,9 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-y-24 p-24">
       <h1 className="font-bold text-amber-400 text-6xl">Passport Data Pages</h1>
+      <div className="bg-amber-300 rounded-sm border-2 border-black flex flex-row justify-center align-center gap-2 p-2 my-4 print:my-0 print:mt-4 w-full md:w-9/12 print:w-9/12 mx-auto break-inside-avoid shadow-blocks-sm font-main">
+        <p className="text-base">yooo whats up dude</p>
+      </div>
       <Playground defaultImageUrl={defaultImageUrl} />
     </main>
   );
