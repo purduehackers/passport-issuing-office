2f0622287398721ac206226b2e82969bae717142
Matthew Stanciu <mattbstanciu@gmail.com>
2024-02-03 05:35:48 -0500

    increase gap  on small screens

--- components/playground.tsx
+++ components/playground.tsx
@@ -100,7 +100,7 @@ export default function Playground({
   }
 
   return (
-    <main className="grid lg:grid-cols-[2fr_3fr] gap-8 lg:gap-12 w-full max-w-4xl">
+    <main className="grid lg:grid-cols-[2fr_3fr] gap-20 lg:gap-12 w-full max-w-4xl">
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
