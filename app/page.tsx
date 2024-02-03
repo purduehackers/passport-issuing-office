80c3702ba67f31f8e26bc019124f55ac3b41a464
Matthew Stanciu <mattbstanciu@gmail.com>
2024-02-03 04:55:50 -0500

    start to add dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -19,10 +19,30 @@ export default async function Home() {
     "data:image/png;base64," + defaultImageBuffer.toString("base64");
 
   return (
-    <main className="flex min-h-screen flex-col items-center gap-y-24 p-24">
-      <h1 className="font-bold text-amber-400 text-6xl">Passport Data Pages</h1>
-      <div className="bg-amber-300 rounded-sm border-2 border-black flex flex-row justify-center align-center gap-2 p-2 my-4 print:my-0 print:mt-4 w-full md:w-9/12 print:w-9/12 mx-auto break-inside-avoid shadow-blocks-sm font-main">
-        <p className="text-base">yooo whats up dude</p>
+    <main className="flex min-h-screen flex-col items-center gap-y-12 sm:gap-y-24 p-4 sm:p-24 bg-slate-900">
+      <div className="bg-slate-200 rounded-sm border-[3px] border-amber-400 flex flex-col justify-center gap-2 p-2 sm:p-4 my-4 print:my-0 print:mt-4 w-full md:w-9/12 print:w-9/12 mx-auto break-inside-avoid shadow-amber-500 shadow-blocks-sm font-main">
+        <div className="flex flex-row items-center">
+          <h1 className="font-bold text-2xl sm:text-6xl mx-auto sm:mb-4 flex justify-center items-center gap-2">
+            <img
+              alt="passport cover"
+              src="/cover-black.svg"
+              className="self-center flex-shrink-0 h-[1em] w-auto pointer-events-none"
+            />
+            Passport Data Pages
+          </h1>
+        </div>
+        <p className="text-base">
+          Use this website to create your passport data page. 🛂
+        </p>
+        <p className="text-base">
+          The info can be whatever you want. Some people have put their real
+          info, others have put totally wacky stuff. Half of the passports have
+          a cat set as their portrait.
+        </p>{" "}
+        <p className="text-base">
+          Once you&#39;ve made your data page, download it and send it to
+          Matthew on Discord (hewillyeah).
+        </p>
       </div>
       <Playground defaultImageUrl={defaultImageUrl} />
     </main>
