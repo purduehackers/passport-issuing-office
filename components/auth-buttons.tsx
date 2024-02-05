fb010a25d57388f02aae5b3bbf13bd5a95993bc3
Matthew Stanciu <mattbstanciu@gmail.com>
2024-02-05 11:23:09 -0500

    remove unused variants

--- components/auth-buttons.tsx
+++ components/auth-buttons.tsx
@@ -1,34 +1,5 @@
 import { signIn, signOut } from "@/auth";
 import { Button } from "./ui/button";
-import { cva, type VariantProps } from "class-variance-authority";
-
-const signInButtonVariants = cva(
-  {
-    variants: {
-      variant: {
-        default: "bg-amber-500 hover:bg-amber-500/90 text-primary-foreground border-2 shadow-blocks-sm",
-        dark: 'border-slate-'
-      },
-      size: {
-        default: "h-10 px-4 py-2",
-        sm: "h-9 rounded-sm px-3",
-        lg: "h-11 rounded-sm px-8",
-        icon: "h-10 w-10",
-        auth: 'h-4 rounded-sm'
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
-    },
-  }
-)
-
-export interface SignInButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof signInButtonVariants> {
-  asChild?: boolean
-}
 
 export function SignOutButton() {
   return (
@@ -57,10 +28,16 @@ export function SignInButton({ dark }: { dark?: boolean }) {
         "use server";
         await signIn("discord");
       }}
-      className={`mx-auto shadow-blocks-tiny ${dark ? 'shadow-discord-light' : 'shadow-discord-deselected'}`}
+      className={`mx-auto shadow-blocks-tiny ${
+        dark ? "shadow-discord-light" : "shadow-discord-deselected"
+      }`}
     >
       <Button
-        className={`${dark ? 'bg-discord-vibrant hover:bg-discord-light hover:text-black' : 'bg-discord-light hover:bg-discord-vibrant'} shadow-none border-none rounded-none h-6`}
+        className={`${
+          dark
+            ? "bg-discord-vibrant hover:bg-discord-light hover:text-black"
+            : "bg-discord-light hover:bg-discord-vibrant"
+        } shadow-none border-none rounded-none h-6`}
         variant="auth"
         type="submit"
       >
