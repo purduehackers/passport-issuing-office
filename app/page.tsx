98c32de0cf30ac5642fdd018456788354a8a6588
Matthew Stanciu <mattbstanciu@gmail.com>
2024-02-05 20:56:26 -0500

    update header copy

--- app/page.tsx
+++ app/page.tsx
@@ -32,18 +32,20 @@ export default async function Home() {
               info, others have put totally wacky stuff. Half of the passports
               have a cat set as their portrait.
             </p>{" "}
-            <p className="text-base">
-              Once you&#39;ve made your data page, download it and send it to
-              Matthew on Discord (hewillyeah).
-            </p>
+            {userId ? (
+              <p className="text-base">
+                When you&#39;re ready to register for a passport ceremony, check
+                the checkbox to be assigned a passport number. Your passport
+                will be printed & ready to assemble at the next ceremony.
+              </p>
+            ) : null}
           </div>
           {!session?.user?.email ? (
             <div className="bg-slate-200 text-black rounded-sm border-[3px] border-red-400 flex flex-col justify-center w-full md:w-7/12 gap-2 p-2 sm:p-4 my-4 mx-auto break-inside-avoid shadow-red-500 shadow-blocks-sm font-main">
               <p>
                 You are currently in Playground Mode: you can play with a data
-                page, and it will not be shared with us. To save your page and
-                register for a passport ceremony,{" "}
-                <span className="font-bold">please sign in.</span>
+                page & it will not be shared with us. To save your page and
+                register for a passport ceremony, you&#39;ll need to sign in.
               </p>
               <SignInButton />
             </div>
