e549fe76dda2a1247687a856e4e6f4124d1c7e5d
Matthew Stanciu <mattbstanciu@gmail.com>
2024-02-09 00:58:36 -0500

    test don't upload full frame

--- components/playground.tsx
+++ components/playground.tsx
@@ -168,28 +168,28 @@ export default function Playground({
 
     let fullFrameFile: File | null = null;
 
-    if (data.sendToDb) {
-      const fullFrameRes: ImageResponse = await fetch(
-        `/api/generate-full-frame`,
-        {
-          method: "POST",
-          body: apiFormData,
-        }
-      );
-      const fullFrameBlob = await fullFrameRes.blob();
-      fullFrameFile = new File([fullFrameBlob], "data_page.png", {
-        type: "image/png",
-      });
-    }
+    // if (data.sendToDb) {
+    //   const fullFrameRes: ImageResponse = await fetch(
+    //     `/api/generate-full-frame`,
+    //     {
+    //       method: "POST",
+    //       body: apiFormData,
+    //     }
+    //   );
+    //   const fullFrameBlob = await fullFrameRes.blob();
+    //   fullFrameFile = new File([fullFrameBlob], "data_page.png", {
+    //     type: "image/png",
+    //   });
+    // }
     updateGenerationStepState("generating", "completed");
 
     if (data.sendToDb) {
       apiFormData.append("generatedImage", generatedImageFile);
-      apiFormData.append("fullFrameImage", fullFrameFile!);
+      // apiFormData.append("fullFrameImage", fullFrameFile!);
 
       await Promise.all([
         uploadImageToR2("generated", apiFormData, generatedPassportNumber),
-        uploadImageToR2("full", apiFormData, generatedPassportNumber),
+        // uploadImageToR2("full", apiFormData, generatedPassportNumber),
       ]);
 
       updateGenerationStepState("uploading", "completed");
