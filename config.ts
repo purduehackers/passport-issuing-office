90600fcccd0b1bb6492d4034e30662f5f94745b3
Matthew Stanciu <mattbstanciu@gmail.com>
2024-02-12 18:51:54 -0500

    bump version to 1

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import { GenerationSteps } from "./types/types";
 
 export const IMAGE_GENERATION_SCALE_FACTOR = 3;
 
-export const CURRENT_PASSPORT_VERSION = 0;
+export const CURRENT_PASSPORT_VERSION = 1;
 
 export const CROP_ASPECT = 0.8;
 
