bec695833fa29a1a14d173db4a8979b0810c3c59
Matthew Stanciu <mattbstanciu@gmail.com>
2024-02-12 15:32:30 -0500

    Interpret date in form as UTC
    I think this might chip away at #30 but I'm not quite sure

--- lib/format-default-date.ts
+++ lib/format-default-date.ts
@@ -1,8 +1,8 @@
 export function formatDefaultDate(isoString: string) {
   const date = new Date(isoString);
   const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getUTCMonth() + 1).padStart(2, "0");
+  const day = String(date.getUTCDate()).padStart(2, "0");
 
   return `${year}-${month}-${day}`;
 }
