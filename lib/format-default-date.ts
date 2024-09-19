0d39f4159da2a6bb218ff6cb4216219dde963bd4
Michael <theshadoweevee@shad.moe>
2024-09-18 20:58:32 -0400

    Add UTC to a year

--- lib/format-default-date.ts
+++ lib/format-default-date.ts
@@ -1,6 +1,6 @@
 export function formatDefaultDate(isoString: string) {
 	const date = new Date(isoString);
-	const year = date.getFullYear();
+	const year = date.getUTCFullYear();
 	const month = String(date.getUTCMonth() + 1).padStart(2, "0");
 	const day = String(date.getUTCDate()).padStart(2, "0");
 
