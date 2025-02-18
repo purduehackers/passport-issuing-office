25895b2ba9029056ad50d97d1d52da8f5c6c14b5
Matthew Stanciu <mattbstanciu@gmail.com>
2025-02-18 18:35:37 -0500

    remove logs + timezoneOffset

--- lib/ceremony-utils.ts
+++ lib/ceremony-utils.ts
@@ -4,21 +4,10 @@ import prisma from "@/lib/prisma";
 import { Ceremony } from "@/types/types";
 
 export async function addNewCeremony(ceremonyData: Ceremony) {
-	console.log({
-		time: new Date(ceremonyData.ceremony_time.getTime()),
-		timezoneOffset: ceremonyData.ceremony_time.getTimezoneOffset() * 60000,
-		calculation: new Date(
-			ceremonyData.ceremony_time.getTime() -
-				ceremonyData.ceremony_time.getTimezoneOffset() * 60000,
-		),
-	});
 	try {
 		await prisma.ceremonies.create({
 			data: {
-				ceremony_time: new Date(
-					ceremonyData.ceremony_time.getTime() -
-						ceremonyData.ceremony_time.getTimezoneOffset() * 60000,
-				),
+				ceremony_time: new Date(ceremonyData.ceremony_time.getTime()),
 				total_slots: ceremonyData.total_slots,
 				open_registration: ceremonyData.open_registration,
 			},
