1c0fb8c08f5e252ed1ba52a6434d1746caeb6f0d
Eric Park <me@ericswpark.com>
2025-11-07 12:44:39 -0500

    fix: use new way of defining single Prisma client
    Referred to:
    https://www.prisma.io/docs/orm/more/help-and-troubleshooting/nextjs-help

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-declare global {
-	var prisma: PrismaClient | undefined;
-}
+const prisma =
+  globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV === "production") {
-	prisma = new PrismaClient();
-} else {
-	if (!global.prisma) {
-		global.prisma = new PrismaClient();
-	}
-	prisma = global.prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
