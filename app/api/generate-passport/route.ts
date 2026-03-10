1f187b1082e5d6d22a1590afe8ea5464edbfd0ec
Kian Kasad <kian@kasad.com>
2026-03-10 12:48:55 -0400

    perf: copy images in R2 instead of re-uploading

--- app/api/generate-passport/route.ts
+++ app/api/generate-passport/route.ts
@@ -1,13 +1,14 @@
 import { generateDataPage, generateFullFrame } from "@/lib/generate-data-page";
 import {
 	generatePreSignedUrl,
+	r2rename,
 	serverR2Download,
 	serverR2Upload,
 } from "@/lib/r2";
 import { GeneratePassportSchema } from "@/types/types";
 import { captureException } from "@sentry/nextjs";
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 
 export async function POST(request: Request) {
 	const parsed = GeneratePassportSchema.safeParse(await request.json());
@@ -75,29 +76,12 @@ export async function POST(request: Request) {
 		const fullFrameFile = await generateFullFrame(dataPageFile);
 
 		if (process.env.PRODUCTION) {
-			let portraitImage: File;
 			try {
-				portraitImage = await serverR2Download(portraitKey, "portrait.png");
-			} catch (error) {
-				captureException(error);
-				return Response.json(
-					{ ok: false, error: "Failed to download portrait from R2" },
-					{ status: 500 },
-				);
-			}
 
-			try {
-				// FIXME: Here, it would be cleaner and faster to move the temporary
-				// datapage and portrait objects that are already in R2 to their
-				// final locations. However, the S3 client uses the FileReader API
-				// for either the Copy or Delete operation, which isn't supported in
-				// the Vercel Edge Runtime. So if we ever switch away from the Edge
-				// runtime, re-implement
-				// https://github.com/purduehackers/passport-issuing-office/commit/c41bca707fc3e36dfd67656c5ac5c97920e8f872
 				await Promise.all([
 					serverR2Upload(`${passportNumber}-full.png`, fullFrameFile),
-					serverR2Upload(`${passportNumber}-portrait.png`, portraitImage),
-					serverR2Upload(`${passportNumber}.png`, dataPageFile),
+					r2rename(dataPageKey, `${passportNumber}.png`),
+					r2rename(portraitKey, `${passportNumber}-portrait.png`),
 				]);
 			} catch (error) {
 				console.log(error);
