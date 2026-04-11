335ae8494327b0fb95f912fc9202ed1843bcf37d
Kian Kasad <kian@kasad.com>
2026-04-11 13:31:09 -0400

    feat: enable Sentry session replay

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -12,6 +12,13 @@ Sentry.init({
   // Enable sending user PII (Personally Identifiable Information)
   // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
+
+  // Session Replay
+  integrations: [
+      Sentry.replayIntegration(),
+  ],
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
